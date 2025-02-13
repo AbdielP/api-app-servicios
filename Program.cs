12a379dbd2f126c22b9431e5688783c2f3462d8d
AbdielP <abdiel.pinzonc@gmail.com>
2025-02-13 10:43:28 -0500

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,29 @@
+var builder = WebApplication.CreateBuilder(args);
+builder.Configuration.AddUserSecrets<Program>();
+
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+//Console.WriteLine($"Cadena de conexión: {connectionString}"); // Solo para verificar que se está cargando
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
